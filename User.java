ce22448ab2529fb2442dd248bc8cf3f7739ef58e
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-03-08 20:37:30 +0530

    [ADDED]: the user input Code

---
+++ User.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class User {
+	public static void main(String[] args)
+	{
+		Scanner x=new Scanner(System.in);
+		int a;
+		a=x.nextInt();
+
+		System.out.println("Enetr the no :"+a);
+
+
+	}
+
+
+}
