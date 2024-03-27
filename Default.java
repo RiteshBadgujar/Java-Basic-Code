8b0c5620726babf15c1770d4adceb4ead1548b31
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-03-27 22:13:16 +0530

    [ADDED]:Ritesh  The code use for Default Constructor call

---
+++ Default.java
@@ -0,0 +1,15 @@
+class Default
+{
+	public void Default()
+	{
+		System.out.println(" Default Constructor call");
+	}
+
+	public static void main(String [] args)
+	{
+
+		Default con = new Default();
+		con.Default();
+
+		}
+}
