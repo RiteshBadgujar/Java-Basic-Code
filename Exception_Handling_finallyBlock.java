397f7677602d0a9d0f14dba3f08f09c5bc8d9d53
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-04-10 21:37:15 +0530

    [ADDED]:Ritesh  The  Exception handing  finaly block in  Runtime

---
+++ Exception_Handling_finallyBlock.java
@@ -0,0 +1,34 @@
+class Exception_Handling_finallyBlock{
+
+	public static void main(String args[]){
+
+		System.out.println("  ---Program Start---  ");
+
+		int a = 10;
+		int b = 0;
+		int c = 0;
+
+		try{
+
+				c = a/b;
+				System.out.println("Division= "+c);
+			}
+
+			catch(ArithmeticException e)
+			{
+
+					System.out.println("Can't Divided by Zero");
+				}
+
+			catch(ArrayIndexOutOfBoundsException s)
+			{
+
+					System.out.println("Array index out of Bound");
+				}
+
+			finally{
+
+					System.out.println("  --- Program end----");
+				}
+		}
+	}
