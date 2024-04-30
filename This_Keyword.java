9c959dbdcf09b844205f4ae402e7057d8bbd891b
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-04-30 21:10:11 +0530

    [ADDED]:Ritesh  The code Using This Keyword

---
+++ This_Keyword.java
@@ -0,0 +1,22 @@
+class This_Keyword{
+
+		int x;
+	    This_Keyword(int x){
+				this.x = x;
+			}
+
+		public static void main(String[] args){
+
+			This_Keyword this_keyword = new This_Keyword(10);
+			System.out.println("Value of X is : "+this_keyword.x);
+
+			}
+	}
+
+
+
+
+
+
+
+
