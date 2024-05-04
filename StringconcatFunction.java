477d684348cfddc446c8d74f851511920e57cbdb
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-05-04 21:44:58 +0530

    [ADDED]:Ritesh  The use String concate function

---
+++ StringconcatFunction.java
@@ -0,0 +1,11 @@
+class StringconcatFunction
+{
+	public static void main(String args[])
+	{
+		String a = "Ritesh";
+		String b = "Badgujar";
+
+		System.out.println("Concatination  = "+a.concat(b));
+
+	}
+}
