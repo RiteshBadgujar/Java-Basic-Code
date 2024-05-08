578ff7c3668be8c78329886779fd73fef62aa5df
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-05-08 21:38:32 +0530

    [ADDED]:Ritesh  The use String Equal function

---
+++ String_equal_Func.java
@@ -0,0 +1,13 @@
+class String_equal_Func
+{
+	public static void main(String args[]) {
+
+		String a = "Ritesh";
+		String b = "Ritesh";
+		String c = "Badgujar";
+
+		System.out.println("String equals = "+a.equals(b));
+		System.out.println("String not equals = "+a.equals(c));
+
+	}
+}
