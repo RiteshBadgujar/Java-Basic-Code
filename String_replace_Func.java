45afee07c989edee7154323b70d4a458afd728d6
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-05-09 21:23:26 +0530

    [ADDED]:Ritesh  The use String Replace function

---
+++ String_replace_Func.java
@@ -0,0 +1,10 @@
+
+class String_replace_Func
+{
+	public static void main(String args[]){
+
+		String s = "Ritesh";
+		System.out.println(" String Replace = "+s.replace("kumar" , "Badgujar"));
+
+	}
+}
