8aeb2e98df3c79db6fa720264b4f386781b2178c
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-05-11 21:36:32 +0530

    [ADDED]:Ritesh  The use String Start with function

---
+++ String_startWith_func.java
@@ -0,0 +1,11 @@
+class String_startWith_func
+{
+	public static void main(String args[]) {
+
+		String a = "Ritesh";
+
+		System.out.println("startsWith() = "+ a.startsWith("hi"));
+		System.out.println("startsWith() = "+ a.startsWith("R"));
+		System.out.println("startsWith() = "+ a.startsWith("sh"));
+	}
+}
