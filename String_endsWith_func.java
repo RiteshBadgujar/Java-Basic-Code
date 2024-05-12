4fc196e732b8041e863bf516ad3b07e386156933
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-05-12 21:09:12 +0530

    [ADDED]:Ritesh  The use String End with function

---
+++ String_endsWith_func.java
@@ -0,0 +1,11 @@
+class String_endsWith_func
+{
+	public static void main(String args[]) {
+
+		String a = "Ritesh Badgujar";
+
+		System.out.println("endsWith() = "+ a.endsWith("sh"));
+		System.out.println("endsWith() = "+ a.endsWith("ba"));
+		System.out.println("endsWith() = "+ a.endsWith("jar"));
+	}
+}
