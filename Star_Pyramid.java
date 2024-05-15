4dd3468f921097ec408fe535d28fe3b86877b04d
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-05-15 21:18:34 +0530

    [ADDED]:Ritesh  The print Star pyramid

---
+++ Star_Pyramid.java
@@ -0,0 +1,16 @@
+class Star_Pyramid
+{
+	public static void main(String args[])
+	{
+		int i,j;
+
+		for(i=10;i>=1;i--)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+	}
+}
